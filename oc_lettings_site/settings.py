ff9cecba2b47d4342d7e9a0ac6f51b2835dc6cb7
BernicheAurelie <berniche.aurelie@orange.fr>
2023-08-31 10:11:36 +0200

    add allowed_hosts with heroku app

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = "fp$9^593hsriajg$_%=5trot9g!1qa@ew(o-1#@=&4%=hp46(s"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ["lettings-site-e28e7f81aa54.herokuapp.com",'127.0.0.1','localhost']
 
 
 # Application definition
