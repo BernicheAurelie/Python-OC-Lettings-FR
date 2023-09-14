237900dfc0b7a805187927f33e00b0661662bace
BernicheAurelie <berniche.aurelie@orange.fr>
2023-09-14 10:33:50 +0200

    modify settings with auth_user_model

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -86,6 +86,7 @@ DATABASES = {
     }
 }
 
+AUTH_USER_MODEL = django.contrib.auth.models.User
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
