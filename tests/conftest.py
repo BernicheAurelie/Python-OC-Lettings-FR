dd7c49c1b3941ccb6e4aefecb246f4d66875af1b
BernicheAurelie <berniche.aurelie@orange.fr>
2023-09-14 15:43:24 +0200

    modify conftest.py (unused)

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,6 @@
-import pytest
+# import pytest
 
 
-@pytest.fixture(autouse=True)
-def enable_db_access_for_all_tests(db):
-    pass
+# @pytest.fixture(autouse=True)
+# def enable_db_access_for_all_tests(db):
+#     pass
