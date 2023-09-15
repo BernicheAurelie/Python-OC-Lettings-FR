3065a9bf4a239e0a53d6ecea322b52d33bbfb9b8
BernicheAurelie <berniche.aurelie@orange.fr>
2023-09-15 07:12:02 +0200

    modify django setup in test_generics_urls

--- tests/unitaries/test_generics_urls.py
+++ tests/unitaries/test_generics_urls.py
@@ -3,12 +3,12 @@ import django
 from django.test import TestCase
 from django.template.loader import render_to_string
 from django.urls import resolve
-# import os
+import os
 
 
-# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "oc_lettings_site.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "oc_lettings_site.settings")
 
-# django.setup()
+django.setup()
 from tests.fixture import TestSetUp
 from oc_lettings_site.views import index
 from profiles.views import index as profiles_index
